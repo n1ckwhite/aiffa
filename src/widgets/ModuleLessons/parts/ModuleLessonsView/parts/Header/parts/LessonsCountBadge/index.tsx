0d6746a61e10d938321e67b77a0ee6f24ff41384
nickwhite <bbycinka@gmail.com>
2025-11-22 12:32:17 +0300

    правки

--- src/widgets/ModuleLessons/parts/ModuleLessonsView/parts/Header/parts/LessonsCountBadge/index.tsx
+++ src/widgets/ModuleLessons/parts/ModuleLessonsView/parts/Header/parts/LessonsCountBadge/index.tsx
@@ -5,9 +5,10 @@ import PillBadge from 'shared/ui/PillBadge';
 
 export const LessonsCountBadge: React.FC<LessonsCountBadgeProps> = ({ count }) => {
   const label = useLessonsLabel(count);
+  const text = `${count} ${label}`;
   return (
     <PillBadge colorScheme="blue">
-      {count} {label}
+      {text}
     </PillBadge>
   );
 };

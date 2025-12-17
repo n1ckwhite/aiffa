59e40d18a43c8a9005b19fb790c4500efbbf8b63
nickwhite <bbycinka@gmail.com>
2025-12-17 11:56:29 +0300

    правки

--- src/widgets/StartCTASection/parts/parts/Actions/Actions.tsx
+++ src/widgets/StartCTASection/parts/parts/Actions/Actions.tsx
@@ -75,7 +75,7 @@ const Actions: React.FC<ActionsProps> = () => {
         w={{ base: '100%', sm: 'auto' }}
         onClick={handleDonate}
       >
-        <Icon viewBox="0 0 24 24" boxSize="1em" mr={2} aria-hidden color={heartIconColor}>
+        <Icon viewBox="0 0 24 24" boxSize="20px" mr={2} aria-hidden color={heartIconColor}>
           <path
             fill="currentColor"
             d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 6 4 4 6.5 4c1.74 0 3.41 1.01 4.22 2.53C11.09 5.01 12.76 4 14.5 4 17 4 19 6 19 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"

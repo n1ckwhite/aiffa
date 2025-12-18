3113ef9024c4965fdfa66ce451852f2f494fbffc
nickwhite <bbycinka@gmail.com>
2025-12-18 13:41:01 +0300

    правки

--- src/widgets/Blog/parts/Screen/parts/BlogArticleCard/parts/BlogArticleCategoryBadge/BlogArticleCategoryBadge.tsx
+++ src/widgets/Blog/parts/Screen/parts/BlogArticleCard/parts/BlogArticleCategoryBadge/BlogArticleCategoryBadge.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Box, HStack, Icon, Text } from "@chakra-ui/react";
 import PillBadge from "shared/ui/PillBadge";
 import { BlogArticleCategoryBadgeProps } from "./types";
 
@@ -7,14 +7,16 @@ import { BlogArticleCategoryBadgeProps } from "./types";
 
 export const BlogArticleCategoryBadge: React.FC<BlogArticleCategoryBadgeProps> = ({ icon, label, categoryColor }) => {
   return (
-    <PillBadge colorScheme="blue" variant="outline" uppercase={false}>
-      <HStack spacing={1.5} align="center">
-        <Icon as={icon} aria-hidden="true" boxSize={4} color={categoryColor} />
-        <Text as="span" color={categoryColor} fontWeight="semibold" noOfLines={1}>
-          {label}
-        </Text>
-      </HStack>
-    </PillBadge>
+    <Box w="fit-content" maxW="100%">
+      <PillBadge colorScheme="blue" variant="outline" uppercase={false}>
+        <HStack as="span" spacing={1.5} align="center" w="fit-content" display="inline-flex">
+          <Icon as={icon} aria-hidden="true" boxSize={4} color={categoryColor} />
+          <Text as="span" color={categoryColor} fontWeight="semibold" noOfLines={1}>
+            {label}
+          </Text>
+        </HStack>
+      </PillBadge>
+    </Box>
   );
 };
 

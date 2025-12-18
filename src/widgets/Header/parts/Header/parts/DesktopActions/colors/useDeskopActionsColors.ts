babe597f21794b0f43b6dea0719ffcf7fdbe658f
nickwhite <bbycinka@gmail.com>
2025-12-18 17:02:29 +0300

    правки

--- src/widgets/Header/parts/Header/parts/DesktopActions/colors/useDeskopActionsColors.ts
+++ src/widgets/Header/parts/Header/parts/DesktopActions/colors/useDeskopActionsColors.ts
@@ -2,9 +2,13 @@ import { useColorModeValue } from '@chakra-ui/react';
 
 export const useDesktopActionsColors = () => {
   const fillIcon = useColorModeValue('blue.700', 'whiteAlpha.900');
+  const avatarBorderColor = useColorModeValue('blackAlpha.200', 'whiteAlpha.200');
+  const avatarBg = useColorModeValue('whiteAlpha.900', 'whiteAlpha.200');
 
     return {
         fillIcon: fillIcon,
+        avatarBorderColor: avatarBorderColor,
+        avatarBg: avatarBg,
     }
 };
 

74e0d890253e0b6d099eaddb9c514e9655f43165
nickwhite <bbycinka@gmail.com>
2026-01-02 15:22:55 +0300

    главная и inter

--- src/shared/fonts/inter.ts
+++ src/shared/fonts/inter.ts
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 
 export const interFont = localFont({
   variable: "--font-inter",
-  display: "swap",
+  display: "block",
   preload: true,
   fallback: [
     "ui-sans-serif",

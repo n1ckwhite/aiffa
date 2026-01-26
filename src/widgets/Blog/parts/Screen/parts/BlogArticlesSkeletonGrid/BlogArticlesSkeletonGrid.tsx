f1c160b8d2a1a10b6ec1c138be2921c5e78e2e71
nickwhite <bbycinka@gmail.com>
2026-01-26 15:05:58 +0300

    правки

--- src/widgets/Blog/parts/Screen/parts/BlogArticlesSkeletonGrid/BlogArticlesSkeletonGrid.tsx
+++ src/widgets/Blog/parts/Screen/parts/BlogArticlesSkeletonGrid/BlogArticlesSkeletonGrid.tsx
@@ -13,7 +13,7 @@ export const BlogArticlesSkeletonGrid: React.FC<BlogArticlesSkeletonGridProps> =
   return (
     <SimpleGrid
       as="ul"
-      columns={{ base: 1, md: 2 }}
+      columns={{ base: 1, md: 2, xl: 3 }}
       spacing={{ base: 6, md: 7 }}
       listStyleType="none"
       m={0}
